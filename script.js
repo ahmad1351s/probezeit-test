97dee565edd880e3751a9201757a26aea3852674
ahmad1351s <minee.ad@gmail.com>
2024-06-27 16:26:11 +0200

    Probezeit1

---
+++ script.js
@@ -0,0 +1,33 @@
+
+function myFunction() {
+    console.log("klick");
+
+const smiley = document.getElementById("smile");
+
+const message = document.getElementById("message");
+
+smiley.src = "foto_smile/emoji-heart-eyes.svg";
+
+message.innerHTML = "Thank you very much, that makes me happy";
+
+message.style.color= "#33cc33";
+
+}
+
+
+function myFunction1() {
+
+    console.log("klick");
+
+const smiley = document.getElementById("smile");
+
+const message = document.getElementById("message");
+
+smiley.src = "foto_smile/emoji-dizzy.svg";
+
+message.innerHTML = "Oh no,i`m sorry to hear that!";
+
+message.style.color= "red";
+
+}
+
